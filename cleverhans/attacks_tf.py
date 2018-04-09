1b1eda1fc87eb0319a7b269d525c963b19af3a1a
YenChenLin <yenchenlin1994@gmail.com>
2018-04-10 06:47:07 +0800

    Copy C&W and change name

--- cleverhans/attacks_tf.py
+++ cleverhans/attacks_tf.py
@@ -866,6 +866,154 @@ class CarliniWagnerL2(object):
         return o_bestattack
 
 
+class STAdv(object):
+
+    def __init__(self, sess, model, batch_size, confidence,
+                 targeted, learning_rate,
+                 binary_search_steps, max_iterations,
+                 abort_early, initial_const,
+                 clip_min, clip_max, num_labels, shape):
+        """
+        Return a tensor that constructs adversarial examples for the given
+        input. Generate uses tf.py_func in order to operate over tensors.
+
+        :param sess: a TF session.
+        :param model: a cleverhans.model.Model object.
+        :param batch_size: Number of attacks to run simultaneously.
+        :param confidence: Confidence of adversarial examples: higher produces
+                           examples with larger l2 distortion, but more
+                           strongly classified as adversarial.
+        :param targeted: boolean controlling the behavior of the adversarial
+                         examples produced. If set to False, they will be
+                         misclassified in any wrong class. If set to True,
+                         they will be misclassified in a chosen target class.
+        :param learning_rate: The learning rate for the attack algorithm.
+                              Smaller values produce better results but are
+                              slower to converge.
+        :param binary_search_steps: The number of times we perform binary
+                                    search to find the optimal tradeoff-
+                                    constant between norm of the purturbation
+                                    and confidence of the classification.
+        :param max_iterations: The maximum number of iterations. Setting this
+                               to a larger value will produce lower distortion
+                               results. Using only a few iterations requires
+                               a larger learning rate, and will produce larger
+                               distortion results.
+        :param abort_early: If true, allows early aborts if gradient descent
+                            is unable to make progress (i.e., gets stuck in
+                            a local minimum).
+        :param initial_const: The initial tradeoff-constant to use to tune the
+                              relative importance of size of the pururbation
+                              and confidence of classification.
+                              If binary_search_steps is large, the initial
+                              constant is not important. A smaller value of
+                              this constant gives lower distortion results.
+        :param clip_min: (optional float) Minimum input component value.
+        :param clip_max: (optional float) Maximum input component value.
+        :param num_labels: the number of classes in the model's output.
+        :param shape: the shape of the model's input tensor.
+        """
+
+        self.sess = sess
+        self.TARGETED = targeted
+        self.LEARNING_RATE = learning_rate
+        self.MAX_ITERATIONS = max_iterations
+        self.BINARY_SEARCH_STEPS = binary_search_steps
+        self.ABORT_EARLY = abort_early
+        self.CONFIDENCE = confidence
+        self.initial_const = initial_const
+        self.batch_size = batch_size
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+        self.model = model
+
+        self.repeat = binary_search_steps >= 10
+
+        self.shape = shape = tuple([batch_size] + list(shape))
+
+        # the variable we're going to optimize over
+        modifier = tf.Variable(np.zeros(shape, dtype=np.float32))
+
+        # these are variables to be more efficient in sending data to tf
+        self.timg = tf.Variable(np.zeros(shape), dtype=tf.float32,
+                                name='timg')
+        self.tlab = tf.Variable(np.zeros((batch_size, num_labels)),
+                                dtype=tf.float32, name='tlab')
+        self.const = tf.Variable(np.zeros(batch_size), dtype=tf.float32,
+                                 name='const')
+
+        # and here's what we use to assign them
+        self.assign_timg = tf.placeholder(tf.float32, shape,
+                                          name='assign_timg')
+        self.assign_tlab = tf.placeholder(tf.float32, (batch_size, num_labels),
+                                          name='assign_tlab')
+        self.assign_const = tf.placeholder(tf.float32, [batch_size],
+                                           name='assign_const')
+
+        # the resulting instance, tanh'd to keep bounded from clip_min
+        # to clip_max
+        self.newimg = (tf.tanh(modifier + self.timg) + 1) / 2
+        self.newimg = self.newimg * (clip_max - clip_min) + clip_min
+
+        # prediction BEFORE-SOFTMAX of the model
+        self.output = model.get_logits(self.newimg)
+
+        # distance to the input data
+        self.other = (tf.tanh(self.timg) + 1) / \
+            2 * (clip_max - clip_min) + clip_min
+        self.l2dist = tf.reduce_sum(tf.square(self.newimg - self.other),
+                                    list(range(1, len(shape))))
+
+        # compute the probability of the label class versus the maximum other
+        real = tf.reduce_sum((self.tlab) * self.output, 1)
+        other = tf.reduce_max(
+            (1 - self.tlab) * self.output - self.tlab * 10000,
+            1)
+
+        if self.TARGETED:
+            # if targeted, optimize for making the other class most likely
+            loss1 = tf.maximum(0.0, other - real + self.CONFIDENCE)
+        else:
+            # if untargeted, optimize for making this class least likely.
+            loss1 = tf.maximum(0.0, real - other + self.CONFIDENCE)
+
+        # sum up the losses
+        self.loss2 = tf.reduce_sum(self.l2dist)
+        self.loss1 = tf.reduce_sum(self.const * loss1)
+        self.loss = self.loss1 + self.loss2
+
+        # Setup the adam optimizer and keep track of variables we're creating
+        start_vars = set(x.name for x in tf.global_variables())
+        optimizer = tf.train.AdamOptimizer(self.LEARNING_RATE)
+        self.train = optimizer.minimize(self.loss, var_list=[modifier])
+        end_vars = tf.global_variables()
+        new_vars = [x for x in end_vars if x.name not in start_vars]
+
+        # these are the variables to initialize when we run
+        self.setup = []
+        self.setup.append(self.timg.assign(self.assign_timg))
+        self.setup.append(self.tlab.assign(self.assign_tlab))
+        self.setup.append(self.const.assign(self.assign_const))
+
+        self.init = tf.variables_initializer(var_list=[modifier] + new_vars)
+
+    def attack(self, imgs, targets):
+        """
+        Perform the L_2 attack on the given instance for the given targets.
+
+        If self.targeted is true, then the targets represents the target labels
+        If self.targeted is false, then targets are the original class labels
+        """
+
+        r = []
+        for i in range(0, len(imgs), self.batch_size):
+            _logger.debug(("Running CWL2 attack on instance " +
+                           "{} of {}").format(i, len(imgs)))
+            r.extend(self.attack_batch(imgs[i:i + self.batch_size],
+                                       targets[i:i + self.batch_size]))
+        return np.array(r)
+
+
 class ElasticNetMethod(object):
 
     def __init__(self, sess, model, beta,
